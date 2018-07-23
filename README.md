1bc1313b15969a277bd05a3ead06eec386418c77
systemed <richard@systemeD.net>
2018-07-23 10:59:29 +0100

    Rework to use SimpleMapnik instead of infamous RubyMapnik

--- README.md
+++ README.md
@@ -3,25 +3,23 @@
 
 Sputnik is a quick-and-dirty way of seeing your changes to a Mapnik stylesheet reflected in real time. It provides a simple tileserver, a full-window map view, and a 'refresh' button which reloads the map using your latest stylesheet.
 
-If you're looking for something less basic, you might want to use [Kosmtik](https://github.com/kosmtik/kosmtik) instead.
-
 ## How
 
 Copy sputnik.html to a web-browsable directory.
 
 Start the server on port 8000 with
 
-    ruby sputnik.rb path_to_your.mml /path/to/directory/with/fonts_and_styles
+    ruby sputnik.rb path_to_your.mml
 
 Open sputnik.html in your web browser.
 
 ## Why
 
-[Tilemill](https://www.mapbox.com/tilemill/) is a beautifully crafted work of wonder and does pretty much everything the Mapnik-toting cartographer could want. Sputnik is not a work of wonder, it's a messy 100-line hack. It does, however, provide a rudimentary alternative to building your own Tilemill if you want latest/hackable Carto features, use your own editor, and are not too enamoured of NodeJS.
+[Tilemill](https://www.mapbox.com/tilemill/) and [Kosmtik](https://github.com/kosmtik/kosmtik) are beautifully crafted works of wonder and do pretty much everything the Mapnik-toting cartographer could want. Sputnik is not a work of wonder, it's a messy 100-line hack. It does, however, provide a rudimentary alternative if you want latest/hackable Carto features, use your own editor, and are fed up of struggling with NodeJS installation.
 
 ## Installation requirements
 
-* Ruby-Mapnik bindings
+* 'Simple Mapnik' bindings (replacing Ruby-Mapnik which was basically uninstallable)
 * Carto (command-line compiler, `npm install -g carto`)
 * Mapnik
 * obvious stuff like Ruby, a webserver, etc. etc.
@@ -32,8 +30,8 @@ First, install `ruby`, `autoconf`, `automake`, `rubygem-cairo-devel`, `boost-dev
 
     npm install -g carto
     gem install rake-compiler rake
-    gem install hoe rice chunky_png cairo
-    gem install ruby_mapnik
+    gem install hoe rice chunky_png cairo rubyzip bundler
+    gem install simple_mapnik
 
 ## Notes
 
@@ -41,13 +39,13 @@ Your map location is bookmarkable, so you can return to the place you were testi
 
 There is a little bit of postprocessing to support [famous gravitystorm list placement hack](https://github.com/mapbox/carto/issues/238).
 
+The XML stylesheet is written to the style directory (as .tmp.xml), ready for upload to your production server.
+
 ## To do
 
 There should be more postprocessing to support comments and macros in the .mml. Hell yes.
 
-Images are generated via the Mapnik::Tile class. This means no metatiling, so (a) slow, (b) label placement is not very representative of your final map.
-
-Error-trapping would be nice.
+Images are generated as individual 256x256 tiles. This means no metatiling, so (a) slow, (b) label placement is not very representative of your final map.
 
 ## Development
 
--- sputnik.rb
+++ sputnik.rb
@@ -1,49 +1,85 @@
 
 	# Sputnik - Style Prototyping Utility for Mapnik
-	# Richard Fairhurst 2014. WTFPL.
-	
+	# Richard Fairhurst 2014/2018. WTFPL.
+
 	require 'webrick'
-	require 'mapnik'
+	require 'simple_mapnik'
+	require 'securerandom'
+	require 'spherical_mercator'
+
+	SimpleMapnik.register_datasources `mapnik-config --input-plugins`.chomp
+	SimpleMapnik.register_fonts `mapnik-config --fonts`.chomp
+	$mercator = SphericalMercator.new(size: 256)
 
 	class MapTile < WEBrick::HTTPServlet::AbstractServlet
 		def do_GET(request, response)
 			_,_,z,x,y=request.path.split('/')
-			tile = Mapnik::Tile.new(z.to_i, x.to_i, y.to_i)
+			bbox_ll = $mercator.bbox(x.to_i, y.to_i, z.to_i)
+			bbox_sm = $mercator.convert(bbox_ll, '900913')
+
 			response.status = 200
 			response['Content-Type']  = 'image/png'
 			response['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
 			response['Pragma']        = 'no-cache'
-			response.body = tile.render_to_string($style.map)
+			$style.map.zoom_to(SimpleMapnik::Bounds.new(*bbox_sm))
+			fn = "/tmp/#{SecureRandom.uuid}.png"
+			$style.map.to_file(fn)
+			response.body = File.read(fn)
+			File.delete(fn)
 		end
+
+		def zoom_x_y_to_lat_lng(zoom, x, y)
+			n = 2.0 ** zoom
+			lon_deg = x / n * 360.0 - 180.0
+			lat_rad = Math.atan(Math.sinh(Math::PI * (1 - 2 * y / n)))
+			lat_deg = lat_rad * 180.0 / Math::PI
+			[lon_deg, lat_deg]
+		end
+	end
+
+	module SimpleMapnik
+	  class Map
+	    def last_error
+	      mapnik_map_last_error @ptr
+	    end
+	  end
 	end
 
 	class Stylesheet
 		attr_reader :xml, :map
-		def initialize(mml,styledir)
+		def initialize(mml)
 			@mml = mml
-			@styledir = styledir
+			@styledir = File.dirname(mml)
 			refresh
 		end
 		def refresh
 			xml = `carto -n #{@mml}`
 			postprocessed = xml.lines.map do |line|
-				if line=~ /^(.+<!\[CDATA\[.+?)(<Placement.+)\]\]>(.+)$/ then
+				if line=~ /^(.*<!\[CDATA\[.+?)(<Placement.+)\]\]>(.+)$/ then
 					pre=$1; exp=$2; post=$3
 					"#{$1}]]>#{$2}#{$3}"
 				else line end
 			end
-			@map = Mapnik::Map.from_xml(postprocessed.join, false, @styledir)
+			Dir.chdir(@styledir) do
+				File.write(".tmp.xml",postprocessed.join)
+				@map = SimpleMapnik::Map.new(256,256)
+				@map.load(".tmp.xml")
+				if (e = map.last_error) then puts e end
+			end
 		end
 	end
 
 	# Start server
 
-	$style = Stylesheet.new(ARGV[0], ARGV[1])
+	$style = Stylesheet.new(ARGV[0])
 	server = WEBrick::HTTPServer.new(:Port => 8000, :AccessLog => [], :Logger => WEBrick::Log::new("/dev/null", 7))
 	server.mount "/tile", MapTile
 	server.mount_proc "/refresh" do |request,response|
 		$style.refresh
 		response.status=200; response['Content-Type']='application/javascript'; response.body="refresh();"
+	end
+	server.mount_proc "/" do |request,response|
+		response.status=200; response['Content-Type']='text/html'; response.body=File.read('sputnik.html')
 	end
 	trap "INT" do server.shutdown end
 	puts "Sputnik launched"
